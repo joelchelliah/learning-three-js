dfcabacb9bae797b0b18822d4a29eaed330e9804
Joel Chelliah <joel.chelliah@bekk.no>
2018-02-24 20:20:40 +0100

    shading

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Scene
 const scene = new THREE.Scene()
 
+
+var clock = new THREE.Clock();
+
 // Camera
 const fov = 75
 const apectRatio = window.innerWidth/window.innerHeight
@@ -8,8 +11,17 @@ const nearPlane = 0.1
 const farPlane = 1000
 const camera = new THREE.PerspectiveCamera( fov, apectRatio, nearPlane, farPlane )
 
-camera.position.z = 5;
+camera.position.y = 3;
+camera.position.z = 10;
+camera.lookAt( scene.position );
 
+// Lights
+const ambLight = new THREE.AmbientLight( "#101010" );
+scene.add( ambLight );
+
+var dirLight = new THREE.DirectionalLight( 0xffaabb );
+dirLight.position.set( -60, 50, 10 ).normalize();
+scene.add( dirLight );
 
 // Renderer
 var renderer = new THREE.WebGLRenderer({antialias:true});
@@ -27,18 +39,18 @@ document.body.appendChild( renderer.domElement );
 // Stuff!
 
 var cube = new THREE.Mesh(
-  new THREE.BoxGeometry( 3, 0.5, 3 ),
-  new THREE.MeshBasicMaterial( { color: "#433FDD" } )
+  new THREE.BoxGeometry( 4, 0.25, 4 ),
+  new THREE.MeshPhongMaterial( { color: 0xaaaaff, flatShading: true, overdraw: 0.5, shininess: 0 } )
 );
 
 var cylinder = new THREE.Mesh(
-  new THREE.CylinderGeometry( 1, 0.5, 2, 32 ),
-  new THREE.MeshBasicMaterial( { color: "#DD2243" } )
+  new THREE.CylinderGeometry( 1, 0.5, 4, 32 ),
+  new THREE.MeshPhongMaterial( { color: 0xaaffaa, flatShading: true, overdraw: 0.5, shininess: 0 } )
 );
 
 var sphere = new THREE.Mesh(
   new THREE.SphereGeometry( 0.5, 32, 32 ),
-  new THREE.MeshBasicMaterial( { color: "#11AA22" } )
+  new THREE.MeshPhongMaterial( { color: 0xffaaaa, flatShading: true, overdraw: 0.5, shininess: 0 } )
 );
 
 sphere.position.y += 2
@@ -49,15 +61,30 @@ scene.add( sphere );
 
 var sphereDir = 0.02
 
+var mouseX = 0, mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+function onDocumentMouseMove( event ) {
+				mouseX = ( event.clientX - windowHalfX );
+				mouseY = ( event.clientY - windowHalfY );
+}
+
 // Render Loop
 var render = function () {
   requestAnimationFrame( render );
+  var elapsedTime = clock.getElapsedTime();
 
-  cube.rotation.y += 0.05;
+  cube.rotation.y += 0.1;
 
   if(sphere.position.y > 2.5 || sphere.position.y < 1.5) sphereDir = -sphereDir
 
-  sphere.position.y += sphereDir
+  sphere.position.y = 3 + Math.sin(elapsedTime)
+
+  camera.position.x += ( mouseX - camera.position.x ) * 0.0001;
+  camera.position.y -= ( mouseY - camera.position.y ) * 0.0001;
 
   // Render the scene
   renderer.render(scene, camera);
